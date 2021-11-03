76e840e2278e07c1be232e8e1a9f8c071fe6f7b6
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-11-03 23:50:30 +0000

    implemented snake movement modifying input.js and play.js

--- input.js
+++ input.js
@@ -1,20 +1,39 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  
+
   stdin.on("data", (data) => {
-    handleUserInput(data);
+    handleUserInput(data, connection);
   });
-  
+
   return stdin;
 };
 
 
-const handleUserInput = (data) => {
-  if (data === '\u0003') {
+const handleUserInput = (data, conn) => {
+  switch (data) {
+  case "\u0003":
     process.exit();
+    break;
+  case "w":
+    conn.write("Move: up");
+    break;
+  case "a":
+    conn.write("Move: left");
+    break;
+  case "s":
+    conn.write("Move: down");
+    break;
+  case "d":
+    conn.write("Move: right");
+    break;
+  default:
+    console.log("invalid key passed");
   }
 };
 
