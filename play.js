6820f8a59cc9e5fdd654ef1cb8ddbaaeaeaa7ea6
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-11-03 23:11:36 +0000

    implemented setupInput.js and handleUserInput.js for play.js

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require("./client");
 
 connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+
+
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput;
